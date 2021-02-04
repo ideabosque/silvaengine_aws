ef3001683285545ba263fc2899b2c8d470ba9274
ideabosque <ideabosque@gmail.com>
2021-02-03 22:35:48 -0800

    Updated the cloudformation script.

--- deployment/cloudformation_stack.py
+++ deployment/cloudformation_stack.py
@@ -58,6 +58,7 @@ class CloudformationStack(object):
         )
         self.aws_lambda = boto3.client(
             'lambda',
+            region_name=os.getenv("region_name"),
             aws_access_key_id=os.getenv('aws_access_key_id'),
             aws_secret_access_key=os.getenv('aws_secret_access_key')
         )
