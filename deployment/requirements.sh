3e63f2636ab0450c9335efced092f10bd59b29b7
ideabosque <ideabosque@gmail.com>
2021-07-23 10:38:25 -0700

    Initial check in.

--- deployment/requirements.sh
+++ deployment/requirements.sh
@@ -1,5 +1,5 @@
 echo "y" | pip uninstall silvaengine_utility && pip install git+git://github.com/ideabosque/silvaengine_utility.git@main#egg=silvaengine_utility
-echo "y" | pip uninstall event_triggers && pip install git+ssh://git@github.com/abacusipllc/event_triggers.git@main#egg=event_triggers
+echo "y" | pip uninstall event_triggers && pip install git+git://github.com/ideabosque/event_triggers.git@main#egg=event_triggers
 echo "y" | pip uninstall silvaengine_base && pip install git+git://github.com/ideabosque/silvaengine_base.git@main#egg=silvaengine_base
 echo "y" | pip uninstall silvaengine_auth && pip install git+git://github.com/ideabosque/silvaengine_auth.git@main#egg=silvaengine_auth
 echo "y" | pip uninstall silvaengine_resource && pip install git+git://github.com/ideabosque/silvaengine_resouces.git@main#egg=silvaengine_resource
