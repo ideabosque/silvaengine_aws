f792b9c7c4f06b1fb29e28665673d4b80f06d28c
Barry Liu <barry.y.liu@hotmail.com>
2022-02-24 15:17:03 +0800

    [U] Update

--- deployment/requirements.sh
+++ deployment/requirements.sh
@@ -2,7 +2,7 @@ pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple/
 
 echo "y" | pip uninstall silvaengine_utility && pip install git+git://github.com/ideabosque/silvaengine_utility.git@main#egg=silvaengine_utility
 echo "y" | pip uninstall event_triggers && pip install git+git://github.com/ideabosque/event_triggers.git@main#egg=event_triggers
-echo "y" | pip uninstall silvaengine_auth && pip install git+git://github.com/ideabosque/silvaengine_auth.git@main#egg=silvaengine_auth
+echo "y" | pip uninstall silvaengine_authorizer && pip install git+git://github.com/ideabosque/silvaengine_authorizer.git@main#egg=silvaengine_authorizer
 echo "y" | pip uninstall silvaengine_base && pip install git+git://github.com/ideabosque/silvaengine_base.git@main#egg=silvaengine_base
 echo "y" | pip uninstall silvaengine_resource && pip install git+git://github.com/ideabosque/silvaengine_resouces.git@main#egg=silvaengine_resource
 
