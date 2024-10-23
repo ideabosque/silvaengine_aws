af319a413246e395ea1ee0559e57633cd2b7e1c7
Idea Bosque <ideabosque@gmail.com>
2024-10-23 08:54:47 -0700

    Update requirements.txt

--- beehive/microcore/workers.py
+++ beehive/microcore/workers.py
@@ -4,12 +4,19 @@ from __future__ import print_function
 
 __author__ = "bibow"
 
-import sys, os
+import os
+import sys
 
 sys.path.append("/opt")
 
-from silvaengine_base import Worker
+EFS_MOUNT_POINT = os.environ.get("EFSMOUNTPOINT")
+PYTHON_PACKAGES_PATH = os.environ.get("PYTHONPACKAGESPATH")
+if EFS_MOUNT_POINT is not None and PYTHON_PACKAGES_PATH is not None:
+    sys.path.append(f"{EFS_MOUNT_POINT}/{PYTHON_PACKAGES_PATH}")
+
 import logging
+
+from silvaengine_base import Worker
 
 logger = logging.getLogger()
 logger.setLevel(eval(os.environ["LOGGINGLEVEL"]))
